fb4f0cf943d9b481f3918ddc457922fa68971940
mattiasminejev <145052004+mattiasminejev@users.noreply.github.com>
2024-04-02 14:24:32 +0300

    yhendasin blackjackiga

--- src/Casino.java
+++ src/Casino.java
@@ -5,14 +5,17 @@ public class Casino {
         System.out.println("Tere tulemast kasiinosse!");
         while (true){
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Millist mängu soovite mängida? (täringud/numbrimäng/exit)");
+            System.out.println("Millist mängu soovite mängida? (täringud/numbrimäng/blackjack/exit)");
             String vastus = scanner.nextLine();
             if (vastus.equalsIgnoreCase("täringud")) {
                 Mäng täringumäng = new Täringumäng();
                 täringumäng.alustaMängu();
             } else if (vastus.equalsIgnoreCase("numbrimäng")) {
-                Mäng numbriMäng = new NumbriMäng(5, 3);
+                Mäng numbriMäng = new NumbriMäng(10, 4);
                 numbriMäng.alustaMängu();
+            }else if (vastus.equalsIgnoreCase("blackjack")) {
+                    Mäng blackjack = new Blackjack();
+                    blackjack.alustaMängu();
             } else if (vastus.equalsIgnoreCase("exit")) {
                 System.out.println("Head aega!");
                 scanner.close();
